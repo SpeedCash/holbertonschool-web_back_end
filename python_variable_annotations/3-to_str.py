1db27e6a0f4c68fce292c6165a2774598f129d73
SpeedCash <tcravero@gmail.com>
2024-04-23 07:03:39 +0000

    3. Basic annotations - to string

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module to convert a float to its string representation.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Converts a float to a string.
+
+    Args:
+    n (float): The float number to convert.
+
+    Returns:
+    str: The string representation of the float.
+    """
+    return str(n)
