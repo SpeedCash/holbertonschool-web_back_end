efff8b9472b46fa31d451f05ef3558b08482a521
SpeedCash <tcravero@gmail.com>
2024-04-23 07:02:53 +0000

    0. Basic annotations - add

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module to add two float numbers and return the result.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two float numbers.
+
+    Args:
+    a (float): The first number.
+    b (float): The second number.
+
+    Returns:
+    float: The sum of a and b.
+    """
+    return a + b
