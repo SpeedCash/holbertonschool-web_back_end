58a9a8db7b4000049c7e8de8a529e83ddd591961
SpeedCash <tcravero@gmail.com>
2024-04-23 07:03:09 +0000

    1. Basic annotations - concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module to concatenate two strings and return the result.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings.
+
+    Args:
+    str1 (str): The first string.
+    str2 (str): The second string.
+
+    Returns:
+    str: The concatenated result of str1 and str2.
+    """
+    return str1 + str2
