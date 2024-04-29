346b2855091c9b6dc3c36023b3f1f1c859b6d8b4
SpeedCash <tcravero@gmail.com>
2024-04-29 13:45:08 +0200

    12 log stats updated

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -5,7 +5,7 @@ from pymongo import MongoClient
 
 
 def log_stats():
-    """Function to print stats about nginx logs."""
+    """Print statistics about nginx logs from MongoDB."""
     client = MongoClient('mongodb://127.0.0.1:27017')
     db = client.logs
     nginx_collection = db.nginx
@@ -17,11 +17,12 @@ def log_stats():
     print("Methods:")
     for method in methods:
         count = nginx_collection.count_documents({"method": method})
-        print(f"{method}: {count}")
+        print(f"method {method}: {count}")
 
-    status_check = nginx_collection.count_documents(
-        {"method": "GET", "path": "/status"})
-    print(f"{status_check} status checks")
+    # Splitting the statement to adhere to the line length limit
+    status_filter = {"method": "GET", "path": "/status"}
+    status_check = nginx_collection.count_documents(status_filter)
+    print(f"{status_check} status check")
 
 
 if __name__ == "__main__":
