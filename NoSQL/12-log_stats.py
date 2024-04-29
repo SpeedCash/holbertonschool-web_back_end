e77c1dadf9f336dafc8c90267797dad8a9b2069d
SpeedCash <tcravero@gmail.com>
2024-04-29 16:53:31 +0200

    12 log updated

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,39 +1,28 @@
 #!/usr/bin/env python3
+"""
+This module contains an asynchronous generator that yields random numbers.
+"""
 
-import pymongo
-
-
-def connect_to_mongo(host="localhost", port=27017,
-                     database="logs", collection="nginx", **kwargs):
-    """Connects to MongoDB."""
-    return pymongo.MongoClient(host, port, **kwargs)[database][collection]
+from pymongo import MongoClient
 
 
-def get_method_counts(collection):
-    """Counts documents for each HTTP method."""
-    pipeline = [{"$group": {"_id": "$method", "count": {"$sum": 1}}}]
-    return {doc['_id']: doc['count'] for doc in collection.aggregate(pipeline)}
+def log_stats():
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client['logs']
+    nginx_collection = db['nginx']
 
-
-def get_status_check_count(collection):
-    """Counts method=GET and path=/status."""
-    return collection.count_documents({"method": "GET", "path": "/status"})
-
-
-def main():
-    """Connects to MongoDB, retrieves and displays statistics."""
-    collection = connect_to_mongo()
-    total_logs = collection.estimated_document_count()
-
+    total_logs = nginx_collection.count_documents({})
     print(f"{total_logs} logs")
+    print("Methods:")
 
-    method_counts = get_method_counts(collection)
-    for method, count in method_counts.items():
-        print(f"\tmethod {method}: {count}")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = nginx_collection.count_documents({"method": method})
+        print(f"    method {method}: {count}")
 
-    status_checks = get_status_check_count(collection)
+    status_checks = nginx_collection.count_documents({"method": "GET", "path": "/status"})
     print(f"{status_checks} status check")
 
 
 if __name__ == "__main__":
-    main()
+    log_stats()
