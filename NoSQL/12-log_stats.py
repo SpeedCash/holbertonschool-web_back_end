e9119dec4ad24ad8c8c52ddbb9d1b4b97c05615f
SpeedCash <tcravero@gmail.com>
2024-04-29 13:32:37 +0200

    files updated

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -17,7 +17,7 @@ def log_stats():
     print("Methods:")
     for method in methods:
         count = nginx_collection.count_documents({"method": method})
-        print(f"    method {method}: {count}")
+        print(f"method {method}: {count}")
 
     status_check = nginx_collection.count_documents
     ({"method": "GET", "path": "/status"})
