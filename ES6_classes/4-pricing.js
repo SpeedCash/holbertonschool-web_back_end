836aa30266a18da701938ba1200051df9b3e4fdc
SpeedCash <tcravero@gmail.com>
2024-05-09 10:29:16 +0200

    updated files with correction from peers

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,39 +1,30 @@
-
-4. Pricing
-mandatory
-
-Import the class Currency from 3-currency.js
-
-Implement a class named Pricing:
-
-    Constructor attributes:
-        amount (Number)
-        currency (Currency)
-    Each attribute must be stored in an “underscore” attribute version (ex: name is stored in _name)
-    Implement a getter and setter for each attribute.
-    Implement a method named displayFullPrice that returns the attributes in the following format amount currency_name (currency_code).
-    Implement a static method named convertPrice. It should accept two arguments: amount (Number), conversionRate (Number). The function should return the amount multiplied by the conversion rate.
-
-bob@dylan:~$ cat 4-main.js
-import Pricing from './4-pricing.js';
-import Currency from './3-currency.js';
-
-const p = new Pricing(100, new Currency("EUR", "Euro"))
-console.log(p);
-console.log(p.displayFullPrice());
-
-bob@dylan:~$ 
-bob@dylan:~$ npm run dev 4-main.js 
-Pricing {
-  _amount: 100,
-  _currency: Currency { _code: 'EUR', _name: 'Euro' }
-}
-100 Euro (EUR)
-bob@dylan:~$ 
-
-Repo:
-
-    GitHub repository: holbertonschool-web_back_end
-    Directory: ES6_classes
-    File: 4-pricing.js
-
+export default class Pricing {
+    constructor(amount, currency) {
+      this._amount = amount;
+      this._currency = currency;
+    }
+  
+    set amount(amount) {
+      this._amount = amount;
+    }
+  
+    get amount() {
+      return this._amount;
+    }
+  
+    set currency(currency) {
+      this._currency = currency;
+    }
+  
+    get currency() {
+      return this._currency;
+    }
+  
+    displayFullPrice() {
+      return `${this.amount} ${this.currency.displayFullCurrency()}`;
+    }
+  
+    static convertPrice(amount, conversionRate) {
+      return amount * conversionRate;
+    }
+  }
