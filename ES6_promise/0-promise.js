51b7528ac7d234ece1f6cfbdf0c894dc1c55b659
SpeedCash <tcravero@gmail.com>
2024-05-13 14:36:13 +0200

    Return a Promise using this prototype function getResponseFromAPI()

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,9 @@
+function getResponseFromAPI() {
+    // Returning a new Promise that resolves to undefined (no specific behavior was defined)
+    return new Promise((resolve) => {
+        resolve();
+    });
+}
+
+// Export the function as required
+export default getResponseFromAPI;
