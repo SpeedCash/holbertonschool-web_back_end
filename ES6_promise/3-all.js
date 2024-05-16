b15d71cf22db52fc249303c983cfeb48b5b5079f
SpeedCash <tcravero@gmail.com>
2024-05-16 19:56:48 +0200

    Update 3-all.js

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,17 +1,13 @@
-// 3-all.js
-import { uploadPhoto, createUser } from './utils.js';
-
+import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
-    .then((results) => {
-      const [photo, user] = results;
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([photo, user]) => {
       console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
     });
 }
-
 
 export default handleProfileSignup;
