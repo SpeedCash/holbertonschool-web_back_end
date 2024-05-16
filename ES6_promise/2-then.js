f7c4a60b8b0915c09900f49ae350af8126edd000
SpeedCash <tcravero@gmail.com>
2024-05-16 15:40:37 +0200

    file updated

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,21 +1,14 @@
 function handleResponseFromAPI(promise) {
-    return promise
-        .then(() => {
-            // Handle successful resolution
-            return {
-                status: 200,
-                body: 'success'
-            };
-        })
-        .catch(() => {
-            // Handle rejection
-            return new Error();
-        })
-        .finally(() => {
-            // Log on every resolution
-            console.log('Got a response from the API');
-        });
-}
-
-// Export the function as required by the project specifications
-export default handleResponseFromAPI;
+    promise
+      .then(() => {
+        console.log('Got a response from the API');
+        return { status: 200, body: 'success' };
+      })
+      .catch(() => {
+        console.log('Got a response from the API');
+        return new Error();
+      });
+  }
+  
+  export default handleResponseFromAPI;
+  
