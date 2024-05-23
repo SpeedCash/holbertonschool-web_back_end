fb6ed45428c2f9a64487279901bac28a93beba8c
SpeedCash <tcravero@gmail.com>
2024-05-23 10:58:46 +0200

    Update 5-typed_arrays.js

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,13 +1,17 @@
-// 5-typed_arrays.js
 export default function createInt8TypedArray(length, position, value) {
-    const buffer = new ArrayBuffer(length);
-    const view = new DataView(buffer);
-  
-    if (position < 0 || position >= length) {
-      throw new Error('Position outside range');
-    }
-  
-    view.setInt8(position, value);
-    return view;
+  // Vérification des paramètres 'length' et 'position' pour s'assurer qu'ils sont valides
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
   }
-  
+
+  // Création d'un ArrayBuffer de la taille spécifiée
+  const buffer = new ArrayBuffer(length);
+  // Création d'une vue DataView pour manipuler le buffer
+  const view = new DataView(buffer);
+
+  // Mise à jour de la valeur à la position spécifiée
+  view.setInt8(position, value);
+
+  // Retourner la vue DataView
+  return view;
+}
