a2daeb7d41cf0f55133b058906934572e206ed0b
SpeedCash <tcravero@gmail.com>
2024-05-23 11:00:52 +0200

    Update 6-set.js

--- ES6_data_manipulation/6-set.js
+++ ES6_data_manipulation/6-set.js
@@ -1,5 +1,9 @@
-// 6-set.js
 export default function setFromArray(arr) {
-    return new Set(arr);
+  // Vérification de l'argument 'arr' pour s'assurer qu'il s'agit d'un tableau
+  if (!Array.isArray(arr)) {
+    throw new TypeError('The argument must be an array');
   }
-  
+
+  // Création d'un Set à partir du tableau
+  return new Set(arr);
+}
