e7067fb2d0ce6a7778ac88e166804cc49e50438d
SpeedCash <tcravero@gmail.com>
2024-05-23 11:07:40 +0200

    Update 9-groceries_list.js

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,11 +1,14 @@
-// 9-groceries_list.js
 export default function groceriesList() {
-    const groceries = new Map();
-    groceries.set('Apples', 10);
-    groceries.set('Tomatoes', 10);
-    groceries.set('Pasta', 1);
-    groceries.set('Rice', 1);
-    groceries.set('Banana', 5);
-    return groceries;
-  }
+  // Création d'une nouvelle instance de Map
+  const groceries = new Map();
   
+  // Ajout des articles d'épicerie et de leurs quantités respectives dans la Map
+  groceries.set('Apples', 10);
+  groceries.set('Tomatoes', 10);
+  groceries.set('Pasta', 1);
+  groceries.set('Rice', 1);
+  groceries.set('Banana', 5);
+  
+  // Retourner la Map contenant les articles d'épicerie
+  return groceries;
+}
