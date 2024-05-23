ef9a191359062befcdf21d1a847bd4bbcbfbcbe3
SpeedCash <tcravero@gmail.com>
2024-05-23 10:55:47 +0200

    Update 4-update_grade_by_city.js

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,10 +1,14 @@
-// 4-update_grade_by_city.js
 export default function updateStudentGradeByCity(students, city, newGrades) {
-    return students
-      .filter(student => student.location === city)
-      .map(student => {
-        const gradeObj = newGrades.find(grade => grade.studentId === student.id);
-        return { ...student, grade: gradeObj ? gradeObj.grade : 'N/A' };
-      });
+  // Vérification des arguments 'students' et 'newGrades' pour s'assurer qu'ils sont des tableaux
+  if (!Array.isArray(students) || !Array.isArray(newGrades)) {
+    throw new TypeError('The arguments students and newGrades must be arrays');
   }
-  
+
+  // Filtrer les étudiants par la ville spécifiée et mettre à jour leurs notes
+  return students
+    .filter(student => student.location === city)
+    .map(student => {
+      const gradeObj = newGrades.find(grade => grade.studentId === student.id);
+      return { ...student, grade: gradeObj ? gradeObj.grade : 'N/A' };
+    });
+}
